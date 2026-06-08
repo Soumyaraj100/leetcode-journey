018f9656add0887c1779822809a0d2d0faf153dd
Soumyaraj Dutta Banik <soumyarajduttabanik@gmail.com>
2026-06-08 23:45:26 +0530

    Time: 142 ms (20.68%) | Memory: 84.2 MB (23.61%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: bool
+        """
+        curr=head
+        m=[]
+        while curr:
+            m.append(curr.val)
+            curr=curr.next
+        return m==m[::-1]
