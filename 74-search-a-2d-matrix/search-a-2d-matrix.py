8841e9cb17013c3546025204c187d6ca8d3ce660
Soumyaraj Dutta Banik <soumyarajduttabanik@gmail.com>
2026-06-14 00:29:36 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (31.21%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        for row in matrix:
+            if row[0] <= target <= row[-1]:
+                left, right = 0, len(row) - 1
+                while left <= right:
+                    mid = (left + right) // 2
+                    if row[mid] == target:
+                        return True
+                    elif row[mid] < target:
+                        left = mid + 1
+                    else:
+                        right = mid - 1
+        return False
