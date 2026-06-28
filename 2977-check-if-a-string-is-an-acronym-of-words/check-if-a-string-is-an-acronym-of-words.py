ebc1e609d1230b6313c935b5662786db60718bc6
Soumyaraj Dutta Banik <soumyarajduttabanik@gmail.com>
2026-06-29 00:20:30 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (55.88%) - LeetSync

---
+++ 2977-check-if-a-string-is-an-acronym-of-words/check-if-a-string-is-an-acronym-of-words.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isAcronym(self, words, s):
+        """
+        :type words: List[str]
+        :type s: str
+        :rtype: bool
+        """
+        acronym = ""
+
+        for word in words:
+            acronym += word[0]
+
+        return acronym == s
