08d70243df0f7496bb54bfbf5679cf5d079e6be6
Soumyaraj Dutta Banik <soumyarajduttabanik@gmail.com>
2026-07-09 04:06:10 +0530

    Time: 11 ms (98.21%) | Memory: 12.5 MB (76.70%) - LeetSync

--- 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.py
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.py
@@ -5,16 +5,10 @@ class Solution(object):
         :type chars: str
         :rtype: int
         """
-        from collections import Counter
-        char_count = Counter(chars)
-        ans = 0
+        res = 0
         for word in words:
-            word_count = Counter(word)
-            good = True
-            for ch in word_count:
-                if word_count[ch] > char_count[ch]:
-                    good = False
-                    break
-            if good:
-                ans += len(word)
-        return ans
+            for i in word:
+                if word.count(i) > chars.count(i): break
+            else:
+                res += len(word)
+        return res
