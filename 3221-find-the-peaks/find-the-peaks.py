192163445d61d61cc6fac5a979d8dd1765ecc65b
Soumyaraj Dutta Banik <soumyarajduttabanik@gmail.com>
2026-07-16 13:31:25 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (22.39%) - LeetSync

---
+++ 3221-find-the-peaks/find-the-peaks.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findPeaks(self, mountain):
+        """
+        :type mountain: List[int]
+        :rtype: List[int]
+        """
+        ans = []
+        for i in xrange(1, len(mountain) - 1):
+            if mountain[i] > mountain[i - 1] and mountain[i] > mountain[i + 1]:
+                ans.append(i)
+        return ans
