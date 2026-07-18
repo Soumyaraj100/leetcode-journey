a1765dd94371ab333442a1c1b2857b60a755babf
Soumyaraj Dutta Banik <soumyarajduttabanik@gmail.com>
2026-07-19 03:24:57 +0530

    Time: 12 ms (17.46%) | Memory: 12.5 MB (61.38%) - LeetSync

---
+++ 4281-score-validator/score-validator.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def scoreValidator(self, events):
+        """
+        :type events: List[str]
+        :rtype: List[int]
+        """
+        s=0
+        c=0
+        for x in events:
+            if x=="W":
+                c+=1
+                if c == 10:
+                    break
+            elif x=="WD" or x=="NB":
+                s+=1
+            else:
+                s+=int(x)
+        return [s,min(c,10)]
