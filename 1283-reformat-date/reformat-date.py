446253608b961b11ac3d791cc6140e2a28004eba
Soumyaraj Dutta Banik <soumyarajduttabanik@gmail.com>
2026-08-09 02:57:19 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (15.25%) - LeetSync

---
+++ 1283-reformat-date/reformat-date.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def reformatDate(self, date):
+        """
+        :type date: str
+        :rtype: str
+        """
+        day, month, year = date.split()
+        months = {
+            "Jan": "01",
+            "Feb": "02",
+            "Mar": "03",
+            "Apr": "04",
+            "May": "05",
+            "Jun": "06",
+            "Jul": "07",
+            "Aug": "08",
+            "Sep": "09",
+            "Oct": "10",
+            "Nov": "11",
+            "Dec": "12"
+        }
+        day = day[:-2]
+        return year + "-" + months[month] + "-" + day.zfill(2)
