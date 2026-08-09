cbdba6ab0c37d93fba2d4ca24d4ecf2456fe1f86
Soumyaraj Dutta Banik <soumyarajduttabanik@gmail.com>
2026-08-10 01:55:50 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (91.67%) - LeetSync

---
+++ 1156-occurrences-after-bigram/occurrences-after-bigram.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def findOcurrences(self, text, first, second):
+        words = text.split()
+        ans = []
+        for i in range(len(words) - 2):
+            if words[i] == first and words[i + 1] == second:
+                ans.append(words[i + 2])
+        return ans
