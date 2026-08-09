37e9ed6b720cbea5311610c5ae8cba69a2031b2a
Soumyaraj Dutta Banik <soumyarajduttabanik@gmail.com>
2026-08-10 00:47:29 +0530

    Time: 3 ms (53.17%) | Memory: 13.3 MB (57.54%) - LeetSync

---
+++ 889-buddy-strings/buddy-strings.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def buddyStrings(self, s, goal):
+        """
+        :type s: str
+        :type goal: str
+        :rtype: bool
+        """
+        if len(s) != len(goal):
+            return False
+        if s == goal:
+            return len(set(s)) < len(s)
+        if sorted(s) != sorted(goal):
+            return False
+        diff = []
+        for i in range(len(s)):
+            if s[i] != goal[i]:
+                diff.append(i)
+        if len(diff) == 2:
+            return True
+        return False
