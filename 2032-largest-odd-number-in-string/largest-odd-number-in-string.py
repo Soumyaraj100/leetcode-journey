35555c209095ef300c034c648b74429d6771cc7e
Soumyaraj Dutta Banik <soumyarajduttabanik@gmail.com>
2026-08-10 01:37:22 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (96.03%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
@@ -1,13 +1,4 @@
 class Solution(object):
     def largestOddNumber(self, num):
-        """
-        :type num: str
-        :rtype: str
-        """
-        while num:
-            if int(num[-1]) % 2 == 0:
-                num = num[:len(num)-1]
-            else:
-                return num
-        return ""
-        
+        return num.rstrip("02468")
+    
